3d00ba351fc63ac75f1552f6f7544130b6174150
Ramadhan Febri Utama <rafeta02@student.uns.ac.id>
2026-09-03 16:02:51 +0700

    sso

--- config/saml.php
+++ config/saml.php
@@ -76,87 +76,87 @@ return [
     ],
 
     // Identity Provider Data that we want connected with our SP.
-    // 'idp' => [
-    //     // Identifier of the IdP entity  (must be a URI)
-    //     'entityId' => env('SAML_IDP_PRIVATEKEY'),
-    //     // SSO endpoint info of the IdP. (Authentication Request protocol)
-    //     'singleSignOnService' => [
-    //         // URL Target of the IdP where the Authentication Request Message
-    //         // will be sent.
-    //         'url' => env('SAML_IDP_SSO_URL', 'https://idp.example.com/idp/profile/SAML2/Redirect/SSO'),
-    //         // SAML protocol binding to be used when returning the <Response>
-    //         // message. OneLogin Toolkit supports the HTTP-Redirect binding
-    //         // only for this endpoint.
-    //         'binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect',
-    //     ],
-    //     // SLO endpoint info of the IdP.
-    //     'singleLogoutService' => [
-    //         // URL Location of the IdP where SLO Request will be sent.
-    //         'url' => env('SAML_IDP_SLS_URL', 'https://idp.example.com/idp/profile/SAML2/Redirect/SLO'),
-    //         // URL location of the IdP where the SP will send the SLO Response (ResponseLocation)
-    //         // if not set, url for the SLO Request will be used
-    //         'responseUrl' => '',
-    //         // SAML protocol binding to be used when returning the <Response>
-    //         // message. OneLogin Toolkit supports the HTTP-Redirect binding
-    //         // only for this endpoint.
-    //         'binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect',
-    //     ],
-    //     // Public x509 certificate of the IdP
-    //     'x509cert' => env('SAML_IDP_X509', ''),
-    //     /*
-    //      *  Instead of use the whole x509cert you can use a fingerprint in order to
-    //      *  validate a SAMLResponse, but we don't recommend to use that
-    //      *  method on production since is exploitable by a collision attack.
-    //      *  (openssl x509 -noout -fingerprint -in "idp.crt" to generate it,
-    //      *   or add for example the -sha256 , -sha384 or -sha512 parameter)
-    //      *
-    //      *  If a fingerprint is provided, then the certFingerprintAlgorithm is required in order to
-    //      *  let the toolkit know which algorithm was used. Possible values: sha1, sha256, sha384 or sha512
-    //      *  'sha1' is the default value.
-    //      *
-    //      *  Notice that if you want to validate any SAML Message sent by the HTTP-Redirect binding, you
-    //      *  will need to provide the whole x509cert.
-    //      */
-    //     // 'certFingerprint' => '',
-    //     // 'certFingerprintAlgorithm' => 'sha1',
+    'idp' => [
+        // Identifier of the IdP entity  (must be a URI)
+        'entityId' => env('SAML_IDP_ENTITYID', 'https://sso.uns.ac.id/saml2/idp/metadata.php'),
+        // SSO endpoint info of the IdP. (Authentication Request protocol)
+        'singleSignOnService' => [
+            // URL Target of the IdP where the Authentication Request Message
+            // will be sent.
+            'url' => env('SAML_IDP_SSO_URL', 'https://sso.uns.ac.id/saml2/idp/SSOService.php'),
+            // SAML protocol binding to be used when returning the <Response>
+            // message. OneLogin Toolkit supports the HTTP-Redirect binding
+            // only for this endpoint.
+            'binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect',
+        ],
+        // SLO endpoint info of the IdP.
+        'singleLogoutService' => [
+            // URL Location of the IdP where SLO Request will be sent.
+            'url' => env('SAML_IDP_SLS_URL', 'https://sso.uns.ac.id/saml2/idp/SingleLogoutService.php'),
+            // URL location of the IdP where the SP will send the SLO Response (ResponseLocation)
+            // if not set, url for the SLO Request will be used
+            'responseUrl' => null,
+            // SAML protocol binding to be used when returning the <Response>
+            // message. OneLogin Toolkit supports the HTTP-Redirect binding
+            // only for this endpoint.
+            'binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect',
+        ],
+        // Public x509 certificate of the IdP
+        'x509cert' => env('SAML_IDP_X509', ''),
+        /*
+         *  Instead of use the whole x509cert you can use a fingerprint in order to
+         *  validate a SAMLResponse, but we don't recommend to use that
+         *  method on production since is exploitable by a collision attack.
+         *  (openssl x509 -noout -fingerprint -in "idp.crt" to generate it,
+         *   or add for example the -sha256 , -sha384 or -sha512 parameter)
+         *
+         *  If a fingerprint is provided, then the certFingerprintAlgorithm is required in order to
+         *  let the toolkit know which algorithm was used. Possible values: sha1, sha256, sha384 or sha512
+         *  'sha1' is the default value.
+         *
+         *  Notice that if you want to validate any SAML Message sent by the HTTP-Redirect binding, you
+         *  will need to provide the whole x509cert.
+         */
+        // 'certFingerprint' => '',
+        // 'certFingerprintAlgorithm' => 'sha1',
 
-    //     /* In some scenarios the IdP uses different certificates for
-    //      * signing/encryption, or is under key rollover phase and
-    //      * more than one certificate is published on IdP metadata.
-    //      * In order to handle that the toolkit offers that parameter.
-    //      * (when used, 'x509cert' and 'certFingerprint' values are
-    //      * ignored).
-    //      */
-    //     // 'x509certMulti' => array(
-    //     //      'signing' => array(
-    //     //          0 => '<cert1-string>',
-    //     //      ),
-    //     //      'encryption' => array(
-    //     //          0 => '<cert2-string>',
-    //     //      )
-    //     // ),
-    // ],
+        /* In some scenarios the IdP uses different certificates for
+         * signing/encryption, or is under key rollover phase and
+         * more than one certificate is published on IdP metadata.
+         * In order to handle that the toolkit offers that parameter.
+         * (when used, 'x509cert' and 'certFingerprint' values are
+         * ignored).
+         */
+        // 'x509certMulti' => array(
+        //      'signing' => array(
+        //          0 => '<cert1-string>',
+        //      ),
+        //      'encryption' => array(
+        //          0 => '<cert2-string>',
+        //      )
+        // ),
+    ],
 
     // Contact information template, it is recommended to supply a
     // technical and support contacts.
-    'contactPerson' => [
-        'technical' => [
-            'givenName' => 'Saml',
-            'emailAddress' => 'saml@example.com',
-        ],
-        'support' => [
-            'givenName' => 'Saml',
-            'emailAddress' => 'saml@example.com',
-        ],
-    ],
+    // 'contactPerson' => [
+    //     'technical' => [
+    //         'givenName' => 'Saml',
+    //         'emailAddress' => 'saml@example.com',
+    //     ],
+    //     'support' => [
+    //         'givenName' => 'Saml',
+    //         'emailAddress' => 'saml@example.com',
+    //     ],
+    // ],
 
     // Organization information template, the info in en_US lang is
     // recomended, add more if required.
     'organization' => [
-        'en-US' => [
-            'name' => 'Saml',
-            'displayname' => 'Saml',
-            'url' => 'http://example.com',
+        'id-ID' => [
+            'name' => 'Universitas Sebelas Maret',
+            'displayname' => 'Universitas Sebelas Maret',
+            'url' => 'https://uns.ac.id',
         ],
     ],
 
